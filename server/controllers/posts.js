d97a9da7b22611344dcaf38a485fc2985c75dc7a
Shiv Kumar Yadav <shivamyadavsy020@gmail.com>
2022-11-22 13:58:52 +0530

    Update posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -9,7 +9,7 @@ export const getPosts = async (req, res) => {
   const { page } = req.query;
   try {
     const LIMIT = 8;
-    const startIndex = (Number(page) - 1) * LIMIT;
+    const startIndex = (Number(page) - 1) *  LIMIT;
     const total = await PostMessage.countDocuments({});
 
     const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
